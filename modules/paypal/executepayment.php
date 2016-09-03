2b24a970dfa6700ae7e5211aca615e9ff6727954
M4LuZ <MaLuZ@gmx.net>
2016-09-03 22:36:28 +0200

    Preparation to allow users to pay for clanmembers via PayPal

--- modules/paypal/executepayment.php
+++ modules/paypal/executepayment.php
@@ -17,13 +17,15 @@ function GetPriceDetails($priceid){
 }
 /*
  * We stored the party_id and the price_id inside the PayPal SKU, so we need to extract it from the string again
+ * We may even have an user ID if this is a payment for someone else
  */
 function SKUtoParty($SKU){
     $SKUarray=explode('-',$SKU);
     if ($SKUarray[0]=='PARTY'){
         return array(
             'party_id' => $SKUarray[1],
-            'price_id' => $SKUarray[2] 
+            'price_id' => $SKUarray[2],
+            'user_id' => $SKUarray[3]
             );
     } else return false;
 }
@@ -66,7 +68,9 @@ if ($PayPalObj->payment->getState()=='created' && !isset($_GET['failed'])){
                            require 'modules/guestlist/class_guestlist.php';
                            $data = SKUtoParty($item->sku);
                            $GuestList = new guestlist();
-                           $GuestList->SetPaid($auth['userid'], $data['party_id']);
+                           if ($data['user_id']>0) { //paid for another user, set him as paid
+                           $GuestList->SetPaid($data['user_id'], $data['party_id']); }
+                           else $GuestList->SetPaid($auth['userid'], $data['party_id']);
                            break;
                    }
                }
--- modules/paypal/paypal.php
+++ modules/paypal/paypal.php
@@ -1,14 +1,15 @@
 <?php
+$dsp->addsinglerow(print_r($auth, true));
 	// GET FORM TO SEE WHAT YOU MUS PAY
 	
 if($auth['userid'] == 0 && $cfg['paypal_donation'] == 0){
-	$func->error(t('Du kannst nichts einzahlen wenn du nicht eingeloggt bist.'),"index.php?mod=home");
+	$func->error(t('Du kannst nichts bezahlen wenn du nicht eingeloggt bist.'),"index.php?mod=home");
 }else{
 	$dsp->NewContent(t('Einzahlen'), t('Hier sehen sie was f&uuml;r Betr&auml;ge noch ausstehend sind. W&auml;hlen sie was sie bezahlen m&ouml;chten.'));
 	$dsp->AddSmartyTpl('javascript', 'paypal');
 	$dsp->SetForm("index.php?mod=paypal&action=CreatePayment","paypal");
 
-	// LIST ALL PARTYS
+	// list all current parties with unpaid entrance fees
 	if($auth['userid'] != 0){
 		$pay_partys = $db->qry("SELECT * FROM %prefix%party_user AS pu LEFT JOIN %prefix%partys AS p USING(party_id) LEFT JOIN %prefix%party_prices AS price ON price.price_id=pu.price_id WHERE user_id=%int% AND paid = '0'", $auth['userid']);
 
@@ -18,11 +19,26 @@ if($auth['userid'] == 0 && $cfg['paypal_donation'] == 0){
 				if($cfg['paypal_depot'] && $pay['depot_price'] > 0){
 					$dsp->AddCheckBoxRow("depot[]",$pay['name'],$pay['depot_text'] . " " . $pay['depot_price'] . " " . $cfg['paypal_currency_code'],"",NULL,NULL,NULL,$pay['price_id']);
 				}
-			}
+			
+                                
+                                
+                                
+                                
+                                }
 		}else{
 			$dsp->AddSingleRow(t('Alle Eintrittspreise bezahlt.'));
 		}
-		
+                //Show also payment options for clanmates, if the user is a clan admin
+                $pay_clanmates = $db->qry('select pu.party_id, u.userid, u.clanid, pu.price_id, u.username from party_user as pu left join user as u on pu.user_id=u.userid where u.clanid=%int% and paid=0', $auth['clanid']);
+                if ($db->num_rows($pay_clanmates > 0)) {
+                    while ($pay = $db->fetch_array($pay_clanmates)){
+                        $dsp->AddCheckBoxRow("clanmates[]",$pay['username'],"",NULL,true,NULL,$pay['userid'].'-'.$pay['price_id']);   
+                    }
+                    
+                } else  {
+                    $dsp->AddSingleRow(t('Keine offenen Zahlungen von Clanmitgliedern.'));
+                    
+                }
 			if($cfg['paypal_catering']){
 				$dsp->AddTextFieldRow("catering",t('Einzahlung f&uuml;r Catering'),0,"");
 			}
